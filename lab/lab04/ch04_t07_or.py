1fdd15fdfa24b4691b60c302cd0d94be2b97a14b
Colin <colin.wan89@gmail.com>
2026-09-10 11:14:37 +0000

    [skip actions] lab/lab04/ch04_t07_or.py

--- lab/lab04/ch04_t07_or.py
+++ lab/lab04/ch04_t07_or.py
@@ -8,13 +8,13 @@ bool_four = None
 
 bool_five = None
 
-Set bool_one equal to the result of
-2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
-Set bool_two equal to the result of
-True or False
-Set bool_three equal to the result of
-100 ** 0.5 >= 50 or False
-Set bool_four equal to the result of
-True or True
-Set bool_five equal to the result of
-1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
+# Set bool_one equal to the result of
+# 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
+# Set bool_two equal to the result of
+# True or False
+# Set bool_three equal to the result of
+# 100 ** 0.5 >= 50 or False
+# Set bool_four equal to the result of
+# True or True
+# Set bool_five equal to the result of
+# 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
