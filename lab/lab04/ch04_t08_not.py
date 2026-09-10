bc9e55a24376641ca9f7725ee3b4c1147d8248d7
Colin <colin.wan89@gmail.com>
2026-09-10 12:06:31 +0000

    [skip actions] lab/lab04/ch04_t08_not.py

--- lab/lab04/ch04_t08_not.py
+++ lab/lab04/ch04_t08_not.py
@@ -1,20 +1,14 @@
+# Set bool_one equal to the result of not True
 bool_one = None
 
+# Set bool_two equal to the result of not 3 ** 4 < 4 ** 3
 bool_two = None
 
+# Set bool_three equal to the result of not 10 % 3 <= 10 % 2
 bool_three = None
 
+# Set bool_four equal to the result of not 3 ** 2 + 4 ** 2 != 5 ** 2
 bool_four = None
 
+# Set bool_five equal to the result of not not False
 bool_five = None
-
-# Set bool_one equal to the result of
-# not True
-# Set bool_two equal to the result of
-# not 3 ** 4 < 4 ** 3
-# Set bool_three equal to the result of
-# not 10 % 3 <= 10 % 2
-# Set bool_four equal to the result of
-# not 3 ** 2 + 4 ** 2 != 5 ** 2
-# Set bool_five equal to the result of
-# not not False
