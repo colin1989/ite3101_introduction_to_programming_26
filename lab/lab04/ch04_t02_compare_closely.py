73a7b4c77a736408bdc8d804b82974e456c45a4f
Colin <colin.wan89@gmail.com>
2026-09-10 11:05:45 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -14,3 +14,9 @@ bool_four = None
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
 bool_five = None
+
+Set bool_one  equal to the result of 17 < 328
+Set bool_two  equal to the result of 100 == (2 * 50)
+Set bool_three  equal to the result of 19 <= 19
+Set bool_four  equal to the result of -22 >= -18
+Set bool_five  equal to the result of 99 != (98 + 1)
