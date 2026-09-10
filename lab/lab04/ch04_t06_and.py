828d5781e01585ae841f027ec5f9d55a0f46ec6a
Colin <colin.wan89@gmail.com>
2026-09-10 11:15:33 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -4,7 +4,7 @@ bool_one = False
 
 
 # Set bool_two equal to the result of -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
-bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
+bool_two = False
 
 bool_three = 19 % 4 != 300 / 10 / 10 and False
 
