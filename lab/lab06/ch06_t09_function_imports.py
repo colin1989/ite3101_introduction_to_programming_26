49f01368cb08f0500a869914a04499a4f1b68dec
Colin <colin.wan89@gmail.com>
2026-09-10 14:01:18 +0000

    [skip actions] lab/lab06/ch06_t09_function_imports.py

--- lab/lab06/ch06_t09_function_imports.py
+++ lab/lab06/ch06_t09_function_imports.py
@@ -1,1 +1,3 @@
 # Import *just* the sqrt function from math on line 3!
+# 1. Let's import only the sqrt function from math this time. (You don't need the () after sqrt in the from math import sqrt bit.)
+import
