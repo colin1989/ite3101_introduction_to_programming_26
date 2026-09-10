64efb53f35f8f15e112f8f895a4638c5ac4fbacb
Colin <colin.wan89@gmail.com>
2026-09-10 14:15:02 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

---
+++ lab/lab06/ch06_t17_review_functions.py
@@ -0,0 +1,9 @@
+def shut_down(s:str):
+    
+
+# 1. First, def a function, shut_down, that takes one argument s. Don't forget the parentheses or the colon!
+
+# Then, if the shut_down function receives an s equal to "yes", it should return "Shutting down"
+# Alternatively, elif s is equal to "no", then the function should return "Shutdown aborted".
+# Finally, if shut_down gets anything other than those inputs, the function should return "Sorry"
+
