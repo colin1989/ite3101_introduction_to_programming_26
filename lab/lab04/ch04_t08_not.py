8d6402446e52006dcc75515cc0495c0c3cd70a88
Colin <colin.wan89@gmail.com>
2026-09-10 12:05:08 +0000

    [skip actions] lab/lab04/ch04_t08_not.py

--- lab/lab04/ch04_t08_not.py
+++ lab/lab04/ch04_t08_not.py
@@ -1,3 +1,4 @@
+# Set bool_one equal to the result of 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
 bool_one = None
 
 bool_two = None
@@ -8,13 +9,11 @@ bool_four = None
 
 bool_five = None
 
-Set bool_one equal to the result of
-2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
-Set bool_two equal to the result of
-True or False
-Set bool_three equal to the result of
-100 ** 0.5 >= 50 or False
-Set bool_four equal to the result of
-True or True
-Set bool_five equal to the result of
-1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
+# Set bool_two equal to the result of
+# True or False
+# Set bool_three equal to the result of
+# 100 ** 0.5 >= 50 or False
+# Set bool_four equal to the result of
+# True or True
+# Set bool_five equal to the result of
+# 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
