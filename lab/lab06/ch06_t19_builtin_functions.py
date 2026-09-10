872854d194586d45955ee163723ab8e0de641f6a
Colin <colin.wan89@gmail.com>
2026-09-10 14:29:55 +0000

    [skip actions] lab/lab06/ch06_t19_builtin_functions.py

--- lab/lab06/ch06_t19_builtin_functions.py
+++ lab/lab06/ch06_t19_builtin_functions.py
@@ -1,4 +1,4 @@
-def distance_from_zero():
+def distance_from_zero(dz):
 # 1. First, def a function called distance_from_zero, with one argument (choose any argument name you like).
 
 # If the type of the argument is either int or float, the function should return the absolute value of the function input.
