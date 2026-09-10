fda266b523e417243f6ad3ac06a36260f8ce08bc
Colin <colin.wan89@gmail.com>
2026-09-10 11:14:53 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -1,3 +1,4 @@
+
 bool_one = False and False
 
 bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
@@ -7,3 +8,12 @@ bool_three = 19 % 4 != 300 / 10 / 10 and False
 bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
 
 bool_five = True and True
+
+Set bool_two equal to the result of
+-(-(-(-2))) == -2 and 4 >= 16 ** 0.5
+Set bool_three equal to the result of
+19 % 4 != 300 / 10 / 10 and False
+Set bool_four equal to the result of
+-(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
+Set bool_five equal to the result of
+True and True
