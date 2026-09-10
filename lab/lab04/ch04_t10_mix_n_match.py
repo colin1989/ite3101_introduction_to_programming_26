3b65fbc4eba0d89e9b888944c5b4d7c379d938c1
Colin <colin.wan89@gmail.com>
2026-09-10 12:23:28 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -15,7 +15,7 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = (2 <= 2) and not "Alpha" == "Bravo"
 
 # Make me false!
-bool_three = None
+bool_three = not not not 2 <= 2
 
 # Make me true!
 bool_four = None
