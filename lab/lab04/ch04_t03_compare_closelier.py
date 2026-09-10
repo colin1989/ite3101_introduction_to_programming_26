b84cff53aee565842a040769c30e0a3e4c17151e
Colin <colin.wan89@gmail.com>
2026-09-10 11:08:43 +0000

    lab/lab04/ch04_t03_compare_closelier.py

--- lab/lab04/ch04_t03_compare_closelier.py
+++ lab/lab04/ch04_t03_compare_closelier.py
@@ -5,13 +5,13 @@ bool_one = (20 - 10) > 15  # We did this one for you!
 
 # (10 + 17) == 3**16
 # Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
-bool_two = None
+bool_two = (10 + 17) == 3**16
 
 # 1**2 <= -1
-bool_three = None
+bool_three = 1**2 <= -1
 
 # 40 * 4 >= -4
-bool_four = None
+bool_four = 40 * 4 >= -4
 
 # 100 != 10**2
-bool_five = None
+bool_five = 100 != 10**2
