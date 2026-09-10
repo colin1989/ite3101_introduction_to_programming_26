2b68770081638d0aab6f5f949f8d6201fb745989
Colin <colin.wan89@gmail.com>
2026-09-10 12:05:58 +0000

    [skip actions] lab/lab04/ch04_t08_not.py

--- lab/lab04/ch04_t08_not.py
+++ lab/lab04/ch04_t08_not.py
@@ -1,4 +1,3 @@
-# Set bool_one equal to the result of 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
 bool_one = None
 
 bool_two = None
@@ -9,11 +8,13 @@ bool_four = None
 
 bool_five = None
 
+# Set bool_one equal to the result of
+# not True
 # Set bool_two equal to the result of
-# True or False
+# not 3 ** 4 < 4 ** 3
 # Set bool_three equal to the result of
-# 100 ** 0.5 >= 50 or False
+# not 10 % 3 <= 10 % 2
 # Set bool_four equal to the result of
-# True or True
+# not 3 ** 2 + 4 ** 2 != 5 ** 2
 # Set bool_five equal to the result of
-# 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
+# not not False
