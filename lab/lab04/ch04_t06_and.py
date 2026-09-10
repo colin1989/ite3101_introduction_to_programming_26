ddad92253b003da98c1e190559c5e16a7368c1d6
Colin <colin.wan89@gmail.com>
2026-09-10 11:15:04 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -1,6 +1,5 @@
 
-# Set bool_one equal to the result of
-# False and False
+# Set bool_one equal to the result of False and False
 bool_one = False and False
 
 bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
