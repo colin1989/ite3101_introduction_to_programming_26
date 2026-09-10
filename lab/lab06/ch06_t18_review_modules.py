a84cd54038b1a2e5eaa0ca743667dd7167be9c4e
Colin <colin.wan89@gmail.com>
2026-09-10 14:18:23 +0000

    lab/lab06/ch06_t18_review_modules.py

--- lab/lab06/ch06_t18_review_modules.py
+++ lab/lab06/ch06_t18_review_modules.py
@@ -1,3 +1,2 @@
 import math
-math.sqrt(13689)
-# 1. Import the math module in whatever way you prefer. Call its sqrt function on the number 13689 and print that value to the console.
+print(math.sqrt(13689))
