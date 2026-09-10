24beca3e67043156a5b9cd698efc804305399017
Colin <colin.wan89@gmail.com>
2026-09-10 13:54:19 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

---
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -0,0 +1,7 @@
+
+
+1. First, def a function called cube that takes an argument called number. Don't forget the parentheses and the colon!
+Make that function return the cube of that number (i.e. that number multiplied by itself and multiplied by itself once again).
+Define a second function called by_three that takes an argument called number.
+if that number is divisible by 3, by_three should call cube(number) and return its result. Otherwise, by_three should return False.
+Don't forget that if and else statements need a  :  at the end of that line!
