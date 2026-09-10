5cf6c19b1f245122f1e8c228a92105a111436a2e
Colin <colin.wan89@gmail.com>
2026-09-10 13:20:08 +0000

    lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -5,6 +5,6 @@ original = input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     print(original)
     word = original.lower()
-    
+    first = word[0]
 else:
     print('empty')
