3e8b2165850dcd501de968373a506ab1a51b026e
Colin <colin.wan89@gmail.com>
2026-09-10 12:25:42 +0000

    [skip actions] lab/lab04/ch04_t11_con_statement_syntax.py

--- lab/lab04/ch04_t11_con_statement_syntax.py
+++ lab/lab04/ch04_t11_con_statement_syntax.py
@@ -7,5 +7,6 @@ if answer == "Left":
 # Will the above print statement print to the console?
 # Set response to 'Y' if you think so, and 'N' if you think not.
 
-# 1. If you think the print statement will print to the console, set response equal to 'Y'; otherwise, set response equal to 'N'.
+# 1. If you think the print statement will print to the console, 
+# set response equal to 'Y'; otherwise, set response equal to 'N'.
 
