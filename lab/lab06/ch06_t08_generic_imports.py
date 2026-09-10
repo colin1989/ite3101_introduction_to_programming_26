15fb992509d4563e09925d570966866ec76b79a2
Colin <colin.wan89@gmail.com>
2026-09-10 13:58:07 +0000

    [skip actions] lab/lab06/ch06_t08_generic_imports.py

--- lab/lab06/ch06_t08_generic_imports.py
+++ lab/lab06/ch06_t08_generic_imports.py
@@ -1,3 +1,8 @@
 # Ask Python to print sqrt(25) on line 3.
 
 print(sqrt(25))
+
+
+
+# Type import math on line 2 in the editor.
+# Insert math. before sqrt() so that it has the form math.sqrt(). This tells Python not only to import math, but to get the sqrt() function from within math. Then hit Run to see what Python now knows.
