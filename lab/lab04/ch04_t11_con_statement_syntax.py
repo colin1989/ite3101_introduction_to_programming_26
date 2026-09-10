eb554c4911874a9f7b85e82127b95cf78bc91550
Colin <colin.wan89@gmail.com>
2026-09-10 12:26:21 +0000

    lab/lab04/ch04_t11_con_statement_syntax.py

--- lab/lab04/ch04_t11_con_statement_syntax.py
+++ lab/lab04/ch04_t11_con_statement_syntax.py
@@ -4,9 +4,6 @@ answer = "Left"
 if answer == "Left":
     print("This is the Verbal Abuse Room, you heap of parrot droppings!")
 
+response = 'Y'
 # Will the above print statement print to the console?
 # Set response to 'Y' if you think so, and 'N' if you think not.
-
-# 1. If you think the print statement will print to the console, 
-# set response equal to 'Y'; otherwise, set response equal to 'N'.
-
