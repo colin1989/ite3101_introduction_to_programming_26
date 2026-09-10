a8f51d39836384d60261339b37573fd06100d0c7
Colin <colin.wan89@gmail.com>
2026-09-10 12:27:17 +0000

    [skip actions] lab/lab04/ch04_t12_if_having.py

--- lab/lab04/ch04_t11_con_statement_syntax.py
+++ lab/lab04/ch04_t11_con_statement_syntax.py
@@ -4,6 +4,6 @@ answer = "Left"
 if answer == "Left":
     print("This is the Verbal Abuse Room, you heap of parrot droppings!")
 
-response = 'Y'
 # Will the above print statement print to the console?
 # Set response to 'Y' if you think so, and 'N' if you think not.
+response = 'Y'
--- lab/lab04/ch04_t12_if_having.py
+++ lab/lab04/ch04_t12_if_having.py
@@ -10,3 +10,10 @@ def using_control_again() -> str:
 
 print(using_control_once())
 print(using_control_again())
+
+1. In the editor you'll see two functions. Don't worry about anything unfamiliar. We'll explain soon enough.
+Replace the underline on line 2 with an expression that returns True.
+Replace the underline on line 6 with an expression that returns True.
+If you do it successfully, then both "Success #1" and "Success #2" are printed.
+
+
