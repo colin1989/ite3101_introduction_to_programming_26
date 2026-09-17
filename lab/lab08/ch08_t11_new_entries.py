eea345496651b9bf520f33fc7bb231922bfbcf1d
Colin <colin.wan89@gmail.com>
2026-09-17 08:32:24 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -3,7 +3,7 @@ menu['Chicken Alfredo'] = 14.50  # Adding new key-value pair
 print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
-
+menu['']
 
 print("There are " + str(len(menu)) + " items on the menu.")
 print(menu)
