fe143c311dd1ed17fc8b9d34402e2840d03a68d5
Colin <colin.wan89@gmail.com>
2026-09-17 08:35:06 +0000

    lab/lab08/ch08_t12_change_yr_mind.py

--- lab/lab08/ch08_t12_change_yr_mind.py
+++ lab/lab08/ch08_t12_change_yr_mind.py
@@ -12,5 +12,5 @@ del zoo_animals['Unicorn']
 # Your code here!
 del zoo_animals['Sloth']
 del zoo_animals['Bengal Tiger']
-zoo_animals['Rockhopper Penguin'] = 
+zoo_animals['Rockhopper Penguin'] = 'Antarctic Exhibit'
 print(zoo_animals)
