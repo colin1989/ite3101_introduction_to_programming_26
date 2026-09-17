82349bef43e96b97d71b5537517733910246ccc6
Colin <colin.wan89@gmail.com>
2026-09-17 07:09:08 +0000

    [skip actions] lab/lab07/ch07_t01_before_we_begin.py

--- lab/lab06/ch06_t19_builtin_functions.py
+++ lab/lab06/ch06_t19_builtin_functions.py
@@ -1,9 +1,5 @@
 def distance_from_zero(param:any):
-    if type(param) == int or type(param) == float:
+    if isinstance(param, (int, float)):
         return abs(param)
     else:
         return "Nope"
-# 1. First, def a function called distance_from_zero, with one argument (choose any argument name you like).
-
-# If the type of the argument is either int or float, the function should return the absolute value of the function input.
-# Otherwise, the function should return "Nope"
