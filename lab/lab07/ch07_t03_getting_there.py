ef23299023108c9c6810bad36204c20b44ff0448
Colin <colin.wan89@gmail.com>
2026-09-17 07:17:41 +0000

    [skip actions] lab/lab07/ch07_t04_transportation.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -1,6 +1,7 @@
 def hotel_cost(nights: int) -> int:
     return 140 * nights
 
+
 def plane_ride_cost(city: str) -> int:
     if "Charlotte" == city:
         return 183
--- lab/lab07/ch07_t04_transportation.py
+++ lab/lab07/ch07_t04_transportation.py
@@ -11,3 +11,7 @@ def plane_ride_cost(city: str) -> int:
         return 222
     elif city == "Los Angeles":
         return 475
+
+
+def rental_car_cost(days: int) -> int:
+    
