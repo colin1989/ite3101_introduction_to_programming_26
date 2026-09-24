0d592a648233c2f377add56b277524c25eb5da2c
Colin <colin.wan89@gmail.com>
2026-09-24 07:15:14 +0000

    [skip actions] lab/lab09/ch09_t08_keep_track_produce.py

--- lab/lab09/ch09_t08_keep_track_produce.py
+++ lab/lab09/ch09_t08_keep_track_produce.py
@@ -15,3 +15,6 @@ for p in prices:
     print("%s" % p)
     print("price: %s" % prices[p])
     print("stock: %s" % stock[p])
+    
+for p in prices:
+    print("%s \n price : %i \n stock : %i" % p, prices[p], stock[i])
