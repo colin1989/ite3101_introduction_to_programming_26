5ef7ea267197756ec89b814173d2f3dfd6adbfe3
Colin <colin.wan89@gmail.com>
2026-09-24 08:34:20 +0000

    [skip actions] lab/lab11/ch011_t16_str_in_list_in_func.py

--- lab/lab11/ch011_t15_iterating_list_in_func.py
+++ lab/lab11/ch011_t15_iterating_list_in_func.py
@@ -2,8 +2,12 @@ from typing import List
 
 n = [3, 5, 7]
 
+
 def total(numbers: List[int]) -> int:
     result = 0
     for n in numbers:
         result += n
+
+    # for i in range(len(list)):
+    #     result += n[i]
     return result
--- lab/lab11/ch011_t16_str_in_list_in_func.py
+++ lab/lab11/ch011_t16_str_in_list_in_func.py
@@ -1,5 +1,5 @@
 n = ["Michael", "Lieberman"]
 # Add your function here
-
+def join_string()
 
 # print(join_strings(n))
