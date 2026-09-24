a5ecdef55d74c62bd54311f3b996da1e6fe2b6a8
Colin <colin.wan89@gmail.com>
2026-09-24 08:02:56 +0000

    lab/lab10/ch010_t09_how_is_everybody_doing.py

--- lab/lab10/ch010_t09_how_is_everybody_doing.py
+++ lab/lab10/ch010_t09_how_is_everybody_doing.py
@@ -65,5 +65,5 @@ def get_class_average(class_list: List[dict]) -> float:
     return average(results)
 
 # Add code below!
-students: List[dict] = [alice, lloyd, tyler]
-p
+students = [alice, lloyd, tyler]
+print(get_class_average(students))
