e6455e023c3aed5840f7404888ae6e3ac3550ac5
Colin <colin.wan89@gmail.com>
2026-09-24 08:17:51 +0000

    lab/lab11/ch011_t06_more_than_one_argu.py

--- lab/lab11/ch011_t06_more_than_one_argu.py
+++ lab/lab11/ch011_t06_more_than_one_argu.py
@@ -1,7 +1,7 @@
 m = 5
 n = 13
 # Add add_function here!
-def add_function(x: int, y: int):
-    x + y
+def add_function(x: int, y: int) -> int:
+    return x + y
 
-# print(add_function(m, n))
+print(add_function(m, n))
