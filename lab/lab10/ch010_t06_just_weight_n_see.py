27cb442884dc7fd3b345aab1f42f0f3f96436ced
Colin <colin.wan89@gmail.com>
2026-09-24 07:51:38 +0000

    [skip actions] lab/lab10/ch010_t06_just_weight_n_see.py

--- lab/lab10/ch010_t06_just_weight_n_see.py
+++ lab/lab10/ch010_t06_just_weight_n_see.py
@@ -25,6 +25,7 @@ def average(numbers: List[float]) -> float:
     return total / len(numbers)
 
 # Add your function below!
-def get_averate(students)
+def get_averate(student)
+    homework = average(student["homework"])
     
     
