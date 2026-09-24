23cc784adf38a2a96098cd9757dd54bf28d1c319
Colin <colin.wan89@gmail.com>
2026-09-24 07:25:21 +0000

    lab/lab09/ch09_t12_stocking_out.py

--- lab/lab09/ch09_t11_making_a_purchase.py
+++ lab/lab09/ch09_t11_making_a_purchase.py
@@ -1,3 +1,5 @@
+from typing import List
+
 shopping_list = ["banana", "orange", "apple"]
 
 stock = {
--- lab/lab09/ch09_t12_stocking_out.py
+++ lab/lab09/ch09_t12_stocking_out.py
@@ -21,5 +21,6 @@ prices = {
 def compute_bill(food: List[str]) -> float:
     total = 0
     for item in food:
-        total += prices[item]
+        if stock[item] > 0:
+            total += prices[item]
     return total
