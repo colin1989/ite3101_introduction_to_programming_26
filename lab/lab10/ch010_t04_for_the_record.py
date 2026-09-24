e87f580f18c777214e81b8b092f8542269a06b6f
Colin <colin.wan89@gmail.com>
2026-09-24 07:37:41 +0000

    lab/lab10/ch010_t04_for_the_record.py

--- lab/lab10/ch010_t04_for_the_record.py
+++ lab/lab10/ch010_t04_for_the_record.py
@@ -18,4 +18,8 @@ tyler = {
 }
 students = [lloyd, alice, tyler]
 
-for
+for student in students:
+    print(student["name"])
+    print(student["homework"])
+    print(student["quizzes"])
+    print(student["tests"])
