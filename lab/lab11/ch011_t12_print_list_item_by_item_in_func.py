025e84c3774de6347a3325a7d4864ad34370df89
Colin <colin.wan89@gmail.com>
2026-09-24 08:27:53 +0000

    [skip actions] lab/lab11/ch011_t13_modi_ele_in_list_in_func.py

--- lab/lab11/ch011_t12_print_list_item_by_item_in_func.py
+++ lab/lab11/ch011_t12_print_list_item_by_item_in_func.py
@@ -1,9 +1,4 @@
-from typing import List
-
 n = [3, 5, 7]
 
 for i in range(0, len(n)):
     print(n[i])
-
-def double_list(x: List[int]) -> List[int]:
-    return x
--- lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
+++ lab/lab11/ch011_t13_modi_ele_in_list_in_func.py
@@ -1,7 +1,12 @@
+from typing import List
+
 n = [3, 5, 7]
 
 for i in range(0, len(n)):
     n[i] = n[i] * 2
 # Don't forget to return your new list!
+
+def double_list(x: List[int]) -> List[int]:
+    return x
 
 # print(double_list(n))
