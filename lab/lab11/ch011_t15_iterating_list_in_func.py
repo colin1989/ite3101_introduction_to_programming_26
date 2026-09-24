e7e0e37fb719558b2fa34404d30af1784b1f0054
Colin <colin.wan89@gmail.com>
2026-09-24 08:33:21 +0000

    lab/lab11/ch011_t15_iterating_list_in_func.py

--- lab/lab11/ch011_t15_iterating_list_in_func.py
+++ lab/lab11/ch011_t15_iterating_list_in_func.py
@@ -2,7 +2,8 @@ from typing import List
 
 n = [3, 5, 7]
 
-def total(numbers: List[int]):
+def total(numbers: List[int]) -> int:
     result = 0
-    for 
+    for n in numbers:
+        result += n
     return result
