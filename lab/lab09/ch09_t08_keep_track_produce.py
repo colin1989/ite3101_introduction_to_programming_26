e12d35709ec9fedfbea7f758fcd2fd58df2c0b94
Colin <colin.wan89@gmail.com>
2026-09-24 07:17:19 +0000

    [skip actions] lab/lab09/ch09_t09_something_value.py

--- lab/lab09/ch09_t08_keep_track_produce.py
+++ lab/lab09/ch09_t08_keep_track_produce.py
@@ -12,10 +12,10 @@ stock = {
 }
 
 
-# for p in prices:
-#     print("%s" % p)
-#     print("price: %s" % prices[p])
-#     print("stock: %s" % stock[p])
-
 for p in prices:
-    print("%s \nprice: %s \nstock: %s" % (p, prices[p], stock[p]))
+    print("%s" % p)
+    print("price: %s" % prices[p])
+    print("stock: %s" % stock[p])
+
+# for p in prices:
+#     print("%s \nprice: %s \nstock: %s" % (p, prices[p], stock[p]))
