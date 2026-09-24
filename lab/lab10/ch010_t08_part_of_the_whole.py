dcc561e9297a2e4fb6e82788afa980965b3f6f56
Colin <colin.wan89@gmail.com>
2026-09-24 08:00:53 +0000

    [skip actions] lab/lab10/ch010_t08_part_of_the_whole.py

--- lab/lab10/ch010_t08_part_of_the_whole.py
+++ lab/lab10/ch010_t08_part_of_the_whole.py
@@ -51,4 +51,4 @@ print(get_letter_grade(get_average(lloyd)))
 def get_class_average(class_list: List[dict]):
     results = []
     for student in class_list:
-        get_average(student)
+        results.append(get_average(student))
